35619b0c894187d2fc49a49a79d654a2c26c9b01
yemrekeskin <yemrekeskin@gmail.com>
2019-12-18 23:58:35 +0300

    import example

---
+++ Lesson2/index.mjs
@@ -0,0 +1,19 @@
+
+// Way --> 1
+
+// import { uppercaseString, lowercaseString } from './string_functions.js';
+
+// console.log(uppercaseString('zsdgfsdf'));
+// console.log(lowercaseString('FFFFFF'));
+
+// Way --> 2
+
+// import * as StringFunc from './string_functions.js';
+
+// console.log(StringFunc.uppercaseString('zsdgfsdf'));
+// console.log(StringFunc.lowercaseString('FFFFFF'));
+
+// file extension .mjs
+// node --experimental-modules index.mjs
+
+
